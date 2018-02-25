bfb1b658df0e340aaa4a4bd94bda5a1593a7b212
Sebastián Pucheta <sebastian.pucheta@gmail.com>
2018-02-25 00:07:41 -0300

    Initial commit.

---
+++ mutate.js
@@ -0,0 +1,37 @@
+function mutate(arrayOrObject, schema, result) {
+	if(typeof(schema) === 'object') {
+		return arrayOrObject.map((el) => {
+			let returnValue = {}
+			for (var key in schema) {
+	    		returnValue[key] = inner_loop(el, schema[key])
+			}
+			return returnValue
+		})
+	}
+}
+
+function inner_loop (element, path) {
+	let prevArr = []
+
+    return path.split('.').reduce((el, key, count, arr) => { 
+		if ( prevArr.length > 0 ) {
+			let returnValue = mapArray(prevArr, key)
+			returnValue = (returnValue !== undefined) ? returnValue : []
+			prevArr = []
+			return returnValue
+		}
+		else if (Array.isArray(el[key])) {
+			prevArr = el[key]
+			return el[key]
+		} 
+		else { return el[key] }
+    }, element)
+}
+
+function mapArray(arr, key) {
+	return arr.map((el) => {
+		return el[key]
+	})
+}
+
+module.exports = mutate
